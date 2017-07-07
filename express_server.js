87766eda93ba922827a08c2cd622b4d480fdb940
Vlad Duta <vlad.du09@gmail.com>
2017-07-07 20:41:08 +0000

    Added code to track unique visitors to each short url and create a log of each visit

--- express_server.js
+++ express_server.js
@@ -42,25 +42,37 @@ function generateRandomString() {
 var urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca",
               userID: "abc123",
-              visits: 0},
+              visits: [ {unique_id: "U-aaa111",
+                        timestamp: "sample-datetime"},
+                        {unique_id: "U-aaa222",
+                        timestamp: "sample-datetime2"}],
+              uniques: 2},
   "Zq4Rt1": {longURL: "http://www.wired.com",
               userID: "abc123",
-              visits: 50},
+              visits: [ {unique_id: "U-bbb111",
+                        timestamp: "sample-datetime3"}],
+              uniques: 1},
   "9sm5xK": {longURL: "http://www.google.com",
               userID: "def456",
-              visits: 200}
+              visits: [ {unique_id: "U-ccc111",
+                        timestamp: "sample-datetime4"},
+                        {unique_id: "U-ddd222",
+                        timestamp: "sample-datetime5"}],
+              uniques: 2}
 };
 
 const users = {
   "abc123": {
     id: "abc123",
     email: "vlad@abc.com",
-    password: hashed_purple
+    password: hashed_purple,
+    unique_id: "U-testID1"
   },
   "def456": {
     id: "def456",
     email: "mike@abc.com",
-    password: hashed_blue
+    password: hashed_blue,
+    unique_id: "U-testID2"
   }
 }
 
@@ -111,10 +123,14 @@ app.post("/register", (req, res) => {
   randomID = generateRandomString()
   // res.cookie('user_id', randomID)
   req.session.user_id = randomID
-  users[randomID] = {id: randomID, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10)}
-  console.log(users)
-  res.redirect("/urls");
 
+  uniqueID = "U-" + generateRandomString()
+  req.session["unique_id"] = uniqueID
+
+  users[randomID] = {id: randomID, email: req.body.email, password: bcrypt.hashSync(req.body.password, 10), unique_id: uniqueID}
+  console.log("TEST - PRINT USERS AFTER REGISTERING: \n\n", users)
+
+  res.redirect("/urls");
 
 })
 
@@ -145,7 +161,12 @@ app.post("/login", (req, res) => {
     //if (users[userID].password === req.body.password) {
     if (bcrypt.compareSync(req.body.password, users[userID].password)) {
       //res.cookie('user_id', users[userID].id)
+      // if (!req.session["unique_id"]) {
+      //   uniqueID = "U-" + generateRandomString()
+      req.session["unique_id"] = users[userID]["unique_id"]
+      // }
       req.session.user_id = users[userID].id
+      console.log("TEST - PRINT USERS AFTER LOGIN: \n\n", users)
       res.redirect("/urls")
     } else {
       res.status(403).send("Your password does not match your email address.")
@@ -219,7 +240,10 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  // debug statement to see POST parameters
   randomString = generateRandomString()
   // urlDatabase[randomString] = {"longURL": req.body.longURL, "userID": req.cookies["user_id"]}
-  urlDatabase[randomString] = {"longURL": req.body.longURL, "userID": req.session["user_id"], "visits": 0}
+  urlDatabase[randomString] = { "longURL": req.body.longURL,
+                                "userID": req.session["user_id"],
+                                "visits": [],
+                                "uniques": 0}
   console.log(urlDatabase)
   // res.send("Ok");         // Respond with 'Ok' (we will replace this)
   res.redirect("http://localhost:8080/urls/" + randomString)
@@ -229,13 +253,35 @@ app.get("/u/:shortURL", (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
     res.status(404).send(`The short URL code you entered (${req.params.shortURL}) does not exist.  Please try again.`)
   }
-  urlDatabase[req.params.shortURL]["visits"] += 1
+  if (!req.session["unique_id"]) {
+    uniqueID = "U-" + generateRandomString()
+    req.session["unique_id"] = uniqueID
+  }
+  newHit = true
+  visitLogs = urlDatabase[req.params.shortURL]["visits"]
+  visitLogs.forEach(function(item, index) {
+    if(item.unique_id === req.session["unique_id"]) {
+      newHit = false
+    }
+  })
+  if (newHit === true) {
+    urlDatabase[req.params.shortURL]["uniques"] += 1
+  }
+
+  //urlDatabase[req.params.shortURL]["uniques"] += 1
+  console.log("TESTING NEW COOKIE ID: ", req.session["unique_id"])
+  a = new Date()
+  a.setHours(a.getHours() - 4)
+  timestamp = new Date(a)
+  timestamp = timestamp.toString().replace("GMT+0000 (UTC)", "EST")
+  urlDatabase[req.params.shortURL]["visits"].push({unique_id: req.session["unique_id"], timestamp: timestamp})
+  console.log(urlDatabase)
+  console.log(urlDatabase[req.params.shortURL]["visits"])
   let finalURL = urlDatabase[req.params.shortURL]["longURL"]
-  console.log(finalURL)
+  // console.log(finalURL)
   // res.render("u_new")
   res.redirect(302, finalURL)
 })
-
 
 
 app.listen(PORT, () => {
